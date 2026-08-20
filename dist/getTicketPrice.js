68e2b466980d7ab5b2368456db1dbe0e55fe9e49
Hridoy-Ahmed-Rafi20 <hridoyahmedrafi20@gmail.com>
2026-08-20 16:45:14 +0600

    get ticket price problem solution complete

--- dist/getTicketPrice.js
+++ dist/getTicketPrice.js
@@ -1,3 +1,27 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const getTicketPrice = (age) => {
+    let price = 0;
+    if (age < 5) {
+        price = 0;
+    }
+    else if (age >= 5 && age <= 12) {
+        price = 100;
+    }
+    else if (age >= 13 && age <= 59) {
+        price = 200;
+    }
+    else {
+        price = 120;
+    }
+    return price;
+};
+console.log(getTicketPrice(3));
+// 0
+console.log(getTicketPrice(10));
+// 100
+console.log(getTicketPrice(25));
+// 200
+console.log(getTicketPrice(65));
+// 120
 //# sourceMappingURL=getTicketPrice.js.map
--- src/getTicketPrice.ts
+++ src/getTicketPrice.ts
@@ -1,3 +1,21 @@
 const getTicketPrice = (age: number): number => {
-  return 0;
+  let price = 0;
+  if (age < 5) {
+    price = 0;
+  } else if (age >= 5 && age <= 12) {
+    price = 100;
+  } else if (age >= 13 && age <= 59) {
+    price = 200;
+  } else {
+    price = 120;
+  }
+  return price;
 };
+getTicketPrice(3);
+// 0
+getTicketPrice(10);
+// 100
+getTicketPrice(25);
+// 200
+getTicketPrice(65);
+// 120

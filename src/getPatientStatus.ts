8dee2838a146eeb788443242ff5ca391708c73de
Hridoy-Ahmed-Rafi20 <hridoyahmedrafi20@gmail.com>
2026-08-20 20:12:58 +0600

    get patient status solution complete

---
+++ src/getPatientStatus.ts
@@ -0,0 +1,59 @@
+type Level = 1 | 2 | 3;
+
+interface GeneralPatient {
+  name: string;
+  age: number;
+  type: "general";
+}
+
+interface EmergencyPatient {
+  name: string;
+  age: number;
+  type: "emergency";
+  emergencyLevel: Level;
+}
+
+// TODO: define types for GeneralPatient and EmergencyPatient
+const getPatientStatus = (
+  patient: GeneralPatient | EmergencyPatient,
+): string => {
+  let status = "";
+
+  if (patient.type === "emergency" && patient.emergencyLevel === 1) {
+    status = "Critical emergency";
+  } else if (patient.type === "emergency" && patient.emergencyLevel === 2) {
+    status = "Serious emergency";
+  } else if (patient.type === "emergency" && patient.emergencyLevel === 3) {
+    status = "Moderate emergency";
+  } else {
+    status = "General patient";
+  }
+  return status;
+};
+getPatientStatus({ name: "Rahim", age: 35, type: "general" });
+// "General patient"
+
+  getPatientStatus({
+    name: "Karim",
+    age: 60,
+    type: "emergency",
+    emergencyLevel: 1,
+  });
+
+
+  getPatientStatus({
+    name: "Rahim",
+    age: 65,
+    type: "emergency",
+    emergencyLevel: 2,
+  });
+
+// "Critical emergency"
+
+  getPatientStatus({
+    name: "Hasan",
+    age: 45,
+    type: "emergency",
+    emergencyLevel: 3,
+  });
+// "Moderate emergency"
